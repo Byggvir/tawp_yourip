ea33a43b87b22cd42a19659b9f863ac3d638faa1
Byggvir <thomas@arend-rhb.de>
2018-06-01 15:42:49 +0200

    Extended IP address information: client, forwarded for and remote address

--- main.php
+++ main.php
@@ -23,20 +23,26 @@ function yourip($atts) {
         )
     );
 
-
+    $ip = ""
+    
     if ( ! empty($_SERVER['HTTP_CLIENT_IP']) ) {
 
         // Return Clients IP Address
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
+        $ip = "Client: $_SERVER['HTTP_CLIENT_IP']";
 
-    } elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+    } 
+    
+    if ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
 
         //check ip from proxy
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        $ip = $ip + "; Forwarded for $_SERVER['HTTP_X_FORWARDED_FOR']";
 
-	} else {
+    }
 
-	$ip = $_SERVER['REMOTE_ADDR'];
+    if ( ! empty( $_SERVER['REMOTE_ADDR'] ) ) {
+        
+        // Remote address
+        $ip = $ip + "; Romote Address: $_SERVER['REMOTE_ADDR']";
 
 	}
 
